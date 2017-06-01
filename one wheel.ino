0671dad5f2175b386a624b4670d4a2bc97ece1f1
ecunningham4 <ecunningham@cca.edu>
2017-05-31 20:54:21 -0700

    Create one wheel.ino
    one wheel continuous motion

---
+++ one wheel.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  
+  pinMode (2,OUTPUT);
+  pinMode (4,OUTPUT);
+  pinMode (6,OUTPUT);
+
+  digitalWrite (2,HIGH);
+  digitalWrite (4,LOW);
+  digitalWrite (6,HIGH);
+
+}
+
+void loop() {
+   
+}
