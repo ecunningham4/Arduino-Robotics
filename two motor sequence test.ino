35d534505198081f2d00d9b85f87e792e2804515
ecunningham4 <ecunningham@cca.edu>
2017-06-06 22:20:29 -0700

    Create two motor sequence test.ino

---
+++ two motor sequence test.ino
@@ -0,0 +1,72 @@
+void setup() 
+{
+  pinMode(2 , OUTPUT);
+  pinMode(3 , OUTPUT);
+  pinMode(4 , OUTPUT);
+  pinMode(5 , OUTPUT);
+
+}
+
+void loop()
+{
+  digitalWrite(2 , HIGH);  //forwrd
+  digitalWrite(3 , LOW);
+  digitalWrite(4 , HIGH);
+  digitalWrite(5 , LOW);
+  delay(1000);
+  digitalWrite(2 , LOW);   //bckwrd
+  digitalWrite(3 , HIGH);
+  digitalWrite(4 , LOW);
+  digitalWrite(5 , HIGH);
+ delay(1000);
+ digitalWrite(2 , LOW);
+  digitalWrite(3 , HIGH);     //right or left
+  digitalWrite(4 , HIGH);
+  digitalWrite(5 , LOW);
+ delay(1000);
+ digitalWrite(2 , HIGH);
+  digitalWrite(3 , LOW);
+  digitalWrite(4 , LOW);        //right or left
+  digitalWrite(5 , HIGH);
+ delay(1000);
+ digitalWrite(2 , LOW);
+  digitalWrite(3 , HIGH);
+  digitalWrite(4 , HIGH);        //circle?
+  digitalWrite(5 , LOW);
+ delay(1000);
+ digitalWrite(2 , LOW);
+  digitalWrite(3 , HIGH);
+  digitalWrite(4 , HIGH);
+  digitalWrite(5 , LOW);          
+ delay(1000);
+ digitalWrite(2 , LOW);
+  digitalWrite(3 , HIGH);
+  digitalWrite(4 , HIGH);
+  digitalWrite(5 , LOW);
+ delay(1000);
+ digitalWrite(2 , LOW);
+  digitalWrite(3 , HIGH);
+  digitalWrite(4 , HIGH);
+  digitalWrite(5 , LOW);
+ delay(1000);
+ digitalWrite(2 , HIGH);
+  digitalWrite(3 , LOW);
+  digitalWrite(4 , LOW);        //circle opposite direction
+  digitalWrite(5 , HIGH);
+ delay(1000);
+ digitalWrite(2 , HIGH);
+  digitalWrite(3 , LOW);
+  digitalWrite(4 , LOW);       
+  digitalWrite(5 , HIGH);
+ delay(1000);
+ digitalWrite(2 , HIGH);
+  digitalWrite(3 , LOW);
+  digitalWrite(4 , LOW);       
+  digitalWrite(5 , HIGH);
+ delay(1000);
+ digitalWrite(2 , HIGH);
+  digitalWrite(3 , LOW);
+  digitalWrite(4 , LOW);        
+  digitalWrite(5 , HIGH);
+ delay(1000);
+}
