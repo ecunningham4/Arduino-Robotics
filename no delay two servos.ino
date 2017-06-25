375c55bac7f32d821b58ac40654d28934912fe1e
ecunningham4 <ecunningham@cca.edu>
2017-06-25 13:56:14 -0700

    Update and rename no delay two servos to no delay two servos.ino

--- no delay two servos.ino
+++ no delay two servos.ino
@@ -1,3 +1,8 @@
+#include "Adafruit_VL53L0X.h"
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+
 //QUADSweep
 //edited from the original code by BARRAGAN
 
@@ -9,16 +14,69 @@ Servo mysecondservo;              // servo 2
 
 int pos = 0;    // variable to store the servo position
 
-void setup()
-{
+
+
+
+
+
+
+
+
+
+
+
+
+void setup() {
+  Serial.begin(115200);
+
+  // wait until serial port opens for native USB devices
+  while (! Serial) {
+    delay(1);
+  }
+  
+  Serial.println("Adafruit VL53L0X test");
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+  // power 
+  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+
+
 myservo.attach(5);  // attaches the servo on pin 9 to the servo object
 mysecondservo.attach(6); // attaches the servo on pin 10 to the servo object
-
+  
 }
 
 
-void loop()
-{
+
+
+
+
+
+
+
+
+
+void loop() {
+  VL53L0X_RangingMeasurementData_t measure;
+    
+  Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println(" out of range ");
+  }
+    
+  delay(100);
+
+
+
+
+
+
 unsigned long start = millis();
 int sweepsNotFinished;
 
@@ -40,5 +98,6 @@ do
     return 0;
   servo.write( startPosition + (endPosition-startPosition) * (currentTime-startTime) / (endTime-startTime));
   return 1;
-  }
 
+ 
+}
